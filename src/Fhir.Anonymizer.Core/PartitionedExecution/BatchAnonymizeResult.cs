25f1382edaf6efaa5adb035308f40c8cb453727d
Tong Wu (SH) <tongwu@microsoft.com>
2020-03-05 15:59:26 +0800

    add missing file

---
+++ src/Fhir.Anonymizer.Core/PartitionedExecution/BatchAnonymizeResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fhir.Anonymizer.Core.PartitionedExecution
+{
+    public class BatchAnonymizeResult
+    {
+        public int Complete { get; set; }
+
+        public int Failed { get; set; }
+    }
+}
