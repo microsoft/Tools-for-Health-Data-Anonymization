ff13e6c9178d29c0847e618b15f7ae84419654d4
Tong Wu (SH) <tongwu@microsoft.com>
2020-03-18 15:08:21 +0800

    add async main

--- src/Fhir.Anonymizer.AzureDataFactoryPipeline/src/Program.cs
+++ src/Fhir.Anonymizer.AzureDataFactoryPipeline/src/Program.cs
@@ -4,11 +4,18 @@ using CommandLine;
 
 namespace Fhir.Anonymizer.DataFactoryTool
 {
+    public class Options
+    {
+        [Option('f', "force", Required = false, HelpText = "Force overwrite the exist blob files in the output container.")]
+        public bool Force { get; set; }
+    }
+
     class Program
     {
         static async Task Main(string[] args)
         {
-            await new DataFactoryCustomActivity().Run().ConfigureAwait(false);
+            await Parser.Default.ParseArguments<Options>(args)
+               .MapResult(async option => await new DataFactoryCustomActivity().Run(option.Force).ConfigureAwait(false), _ => Task.FromResult(1)).ConfigureAwait(false);
         }
     }
 }
