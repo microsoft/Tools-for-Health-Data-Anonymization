622249cfb595edf2e3ce8c8c2359b745e1b4e8d1
Tong Wu (SH) <tongwu@microsoft.com>
2020-04-20 16:57:25 +0800

    address comments

---
+++ src/Fhir.Anonymizer.Tool/AnonymizationToolOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fhir.Anonymizer.Tool
+{
+    public class AnonymizationToolOptions
+    {
+        public bool IsRecursive { get; set; }
+        public bool ValidateInput { get; set; }
+        public bool ValidateOutput { get; set; }
+        public bool SkipExistedFile { get; set; }
+    }
+}
