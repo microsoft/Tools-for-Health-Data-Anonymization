9ee0014229f2ab32dd4d67fc3a44c1234b2ddad1
skywalker-wu <tong.wu@live.com>
2020-04-21 15:43:09 +0800

    Support resume on anonymization tool
    1. Support json/ndjson file resume on local tool
    2. Support json/ndjson file resume on ADF pipeline

--- src/Fhir.Anonymizer.Tool/Program.cs
+++ src/Fhir.Anonymizer.Tool/Program.cs
@@ -7,77 +7,34 @@ using Microsoft.Extensions.Logging;
 
 namespace Fhir.Anonymizer.Tool    
 {
+    class Options
+    {
+        [Option('i', "inputFolder", Required = true, HelpText = "Folder to locate input resource files.")]
+        public string InputFolder { get; set; }
+        [Option('o', "outputFolder", Required = true, HelpText = "Folder to save anonymized resource files.")]
+        public string OutputFolder { get; set; }
+        [Option('c', "configFile", Required = false, Default = "configuration-sample.json", HelpText = "Anonymizer configuration file path.")]
+        public string ConfigurationFilePath { get; set; }
+        [Option('b', "bulkData", Required = false, Default = false, HelpText = "Resource file is in bulk data format (.ndjson).")]
+        public bool IsBulkData { get; set; }
+        [Option('s', "skip", Required = false, Default = false, HelpText = "Skip existed files in target folder.")]
+        public bool SkipExistedFile { get; set; }
+        [Option('r', "recursive", Required = false, Default = false, HelpText = "Process resource files in input folder recursively.")]
+        public bool IsRecursive { get; set; }
+        [Option('v', "verbose", Required = false, Default = false, HelpText = "Provide additional details in processing.")]
+        public bool IsVerbose { get; set; }
+        [Option("validateInput", Required = false, Default = false, HelpText = "Validate input resources. Details can be found in verbose log.")]
+        public bool ValidateInput { get; set; }
+        [Option("validateOutput", Required = false, Default = false, HelpText = "Validate anonymized resources. Details can be found in verbose log.")]
+        public bool ValidateOutput { get; set; }
+    }
+
     public class Program
     {
-        class Options
-        {
-            [Option('i', "inputFolder", Required = true, HelpText = "Folder to locate input resource files.")]
-            public string InputFolder { get; set; }
-            [Option('o', "outputFolder", Required = true, HelpText = "Folder to save anonymized resource files.")]
-            public string OutputFolder { get; set; }
-            [Option('c', "configFile", Required = false, Default = "configuration-sample.json", HelpText = "Anonymizer configuration file path.")]
-            public string ConfigurationFilePath { get; set; }
-            [Option('b', "bulkData", Required = false, Default = false, HelpText = "Resource file is in bulk data format (.ndjson).")]
-            public bool IsBulkData { get; set; }
-            [Option('r', "recursive", Required = false, Default = false, HelpText = "Process resource files in input folder recursively.")]
-            public bool IsRecursive { get; set; }
-            [Option('v', "verbose", Required = false, Default = false, HelpText = "Provide additional details in processing.")]
-            public bool IsVerbose { get; set; }
-            [Option("validateInput", Required = false, Default = false, HelpText = "Validate input resources. Details can be found in verbose log.")]
-            public bool ValidateInput { get; set; }
-            [Option("validateOutput", Required = false, Default = false, HelpText = "Validate anonymized resources. Details can be found in verbose log.")]
-            public bool ValidateOutput { get; set; }
-        }
-
         public async static Task Main(string[] args)
         {
             await CommandLine.Parser.Default.ParseArguments<Options>(args)
-               .MapResult(async options => await RunAnonymization(options).ConfigureAwait(false), _ => Task.FromResult(1)).ConfigureAwait(false);
-        }
-
-        private async static Task RunAnonymization(Options options)
-        {
-            try
-            {
-                InitializeAnonymizerLogging(options.IsVerbose);
-
-                var dataProcessor = new FhirResourceDataProcessor(options.ConfigurationFilePath);
-                if (dataProcessor.IsSameDirectory(options.InputFolder, options.OutputFolder))
-                {
-                    throw new Exception("Input and output folders are the same! Please choose another folder.");
-                }
-
-                Directory.CreateDirectory(options.OutputFolder);
-
-                if (options.IsBulkData)
-                {
-                    await dataProcessor.AnonymizeBulkDataFolder(options.InputFolder, options.OutputFolder, options.IsRecursive, options.ValidateInput, options.ValidateOutput).ConfigureAwait(false);
-                }
-                else
-                {
-                    await dataProcessor.AnonymizeFolder(options.InputFolder, options.OutputFolder, options.IsRecursive, options.ValidateInput, options.ValidateOutput).ConfigureAwait(false);
-                }
-            }
-            finally
-            {
-                // Dispose to flush logs
-                DisposeAnonymizerLogging();
-            }
-        }
-
-        private static void InitializeAnonymizerLogging(bool isVerboseMode)
-        {
-            AnonymizerLogging.LoggerFactory = LoggerFactory.Create(builder => {
-                builder.AddFilter("Microsoft", LogLevel.Warning)
-                       .AddFilter("System", LogLevel.Warning)
-                       .AddFilter("Fhir.Anonymizer", isVerboseMode ? LogLevel.Trace : LogLevel.Information)
-                       .AddConsole();
-            });
-        }
-
-        private static void DisposeAnonymizerLogging()
-        {
-            AnonymizerLogging.LoggerFactory.Dispose();
-        }
+               .MapResult(async options => await AnonymizationLogic.AnonymizeAsync(options).ConfigureAwait(false), _ => Task.FromResult(1)).ConfigureAwait(false);
+        }        
     }
 }
