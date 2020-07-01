a355b8997143cfead173536459613f58c50a5158
Yue Fei <yufei@microsoft.com>
2020-07-01 12:33:34 +0800

    Add validation for encryption key

--- src/Fhir.Anonymizer.Core/AnonymizationConfigurations/AnonymizerConfigurationValidator.cs
+++ src/Fhir.Anonymizer.Core/AnonymizationConfigurations/AnonymizerConfigurationValidator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Text.RegularExpressions;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using Hl7.FhirPath;
 
 namespace Fhir.Anonymizer.Core.AnonymizerConfigurations
@@ -38,6 +40,51 @@ namespace Fhir.Anonymizer.Core.AnonymizerConfigurations
                     throw new AnonymizerConfigurationErrorsException($"{method} not support.");
                 }
             }
+
+            // Check AES key size is valid (16, 24 or 32 bytes).
+            if (!string.IsNullOrEmpty(config.ParameterConfiguration?.EncryptKey))
+            {
+                using Aes aes = Aes.Create();
+                var encryptKeySize = Encoding.UTF8.GetByteCount(config.ParameterConfiguration.EncryptKey) * 8;
+                if (!IsValidKeySize(encryptKeySize, aes.LegalKeySizes))
+                {
+                    throw new AnonymizerConfigurationErrorsException($"Invalid encrypt key size : {encryptKeySize} bits! Please provide key sizes of 128, 192 or 256 bits.");
+                }
+            }
+
         }
+
+        // The following method takes a bit length input and returns whether that length is a valid size
+        // validSizes for AES: MinSize=128, MaxSize=256, SkipSize=64
+        private bool IsValidKeySize(int bitLength, KeySizes[] validSizes)
+        {
+            int i, j;
+
+            if (validSizes == null) return false;
+            for (i = 0; i < validSizes.Length; i++)
+            {
+                if (validSizes[i].SkipSize == 0)
+                {
+                    // assume MinSize = MaxSize
+                    if (validSizes[i].MinSize == bitLength)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    for (j = validSizes[i].MinSize; j <= validSizes[i].MaxSize; j += validSizes[i].SkipSize)
+                    {
+                        if (j == bitLength)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
     }
 }
