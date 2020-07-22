e3a4636df3c3ecda6ca663d371037a22f5498eb8
feiyue <>
2020-07-22 13:45:21 +0800

    Add perturb processor

---
+++ src/Fhir.Anonymizer.Shared.Core/Processors/Settings/PerturbSetting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using EnsureThat;
+
+namespace Fhir.Anonymizer.Core.Processors.Settings
+{
+    public class PerturbSetting
+    {
+        public decimal Span { get; set; }
+        public PerturbRangeType RangeType { get; set; }
+        public int RoundTo { get; set; }
+
+        public static PerturbSetting CreateFromRuleSettings(Dictionary<string, object> ruleSettings)
+        {
+            EnsureArg.IsNotNull(ruleSettings);
+
+            var roundTo = 2;
+            if (ruleSettings.ContainsKey(RuleKeys.RoundTo)) 
+            {
+                roundTo = Convert.ToInt32(ruleSettings.GetValueOrDefault(RuleKeys.RoundTo)?.ToString());
+            }
+
+            var span = 0;
+            if (ruleSettings.ContainsKey(RuleKeys.Span))
+            {
+                span = Convert.ToDecimal(ruleSettings.GetValueOrDefault(RuleKeys.Span)?.ToString());
+            }
+
+            var rangeType = PerturbRangeType.Fixed;
+            if (string.Equals(PerturbRangeType.Proportional.ToString(), ruleSettings.GetValueOrDefault(RuleKeys.RangeType)?.ToString()))
+            {
+                rangeType = PerturbRangeType.Proportional;
+            }
+
+            return new PerturbSetting
+            {
+                Span = span,
+                RangeType = rangeType,
+                RoundTo = roundTo
+            };
+        }
+    }
+}
---
+++ src/Fhir.Anonymizer.Shared.Core/Processors/Settings/RuleKeys.cs
@@ -0,0 +1,11 @@
+namespace Fhir.Anonymizer.Core.Processors.Settings
+{
+    internal static class RuleKeys 
+    {
+        internal const string ReplaceWith = "replaceWith";
+        internal const string RangeType = "rangeType";
+        internal const int RoundTo = "roundTo";
+        internal const int Span = "span";
+        internal const int ScaleType = "scaleType";
+    }
+}
