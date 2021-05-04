44316d274a9c9100a56458c1c05a19b38ec56386
sharon <sharon.hart@microsoft.com>
2021-05-04 17:02:43 +0300

    Presidio integration with stubs

---
+++ src/Microsoft.Health.Fhir.Anonymizer.Shared.Core/Utility/PresidioUtility.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Microsoft.Health.Fhir.Anonymizer.Core.Utility
+{
+    public class PresidioUtility
+    {
+        public static string AnalyzeAndAnonymize(string text)
+        {
+            var analyzerResult = Analyze(text);
+            var anonymizerResponse = Anonymize(text, analyzerResult);
+            return anonymizerResponse.Text;
+        }
+
+
+        private static List<AnalyzerResult> Analyze(string text)
+        {
+            // Call Analyzer REST API
+            return new List<AnalyzerResult>(new[] {new AnalyzerResult()});
+        }
+        
+        private static AnonymizeResponse Anonymize(string text, List<AnalyzerResult> analyzerResult)
+        {
+            // Call Anonymizer REST API
+            var anonymizeResponse = new AnonymizeResponse();
+            anonymizeResponse.Text = "Anonymized Text";
+            return anonymizeResponse;
+        }
+        
+
+    }
+    
+    //TODO: [ADO-3166] Stub, replace with the actual SDK object.
+    public class AnalyzerResult
+    {
+    }
+
+    //TODO: [ADO-3166] Stub, replace with the actual SDK object.
+    public class AnonymizeResponse
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+        
+    }
+
+
+}
