c4a6979c14af358756292080707c5113bd97c1c8
Roel Fauconnier <roel.fauconnier@microsoft.com>
2021-05-18 16:10:52 +0200

    remove unnecessary globalsetup from program.cs

--- src/Microsoft.Health.Fhir.Anonymizer.Stu3.Benchmarks/Program.cs
+++ src/Microsoft.Health.Fhir.Anonymizer.Stu3.Benchmarks/Program.cs
@@ -1,31 +1,9 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-using Microsoft.Extensions.Logging;
-using Microsoft.Health.Fhir.Anonymizer.Core;
-using System;
+﻿using BenchmarkDotNet.Running;
 
 namespace Microsoft.Health.Fhir.Anonymizer.Benchmarks
 {
     public class Program
     {
-        private readonly ILogger _logger = AnonymizerLogging.CreateLogger<Program>();
-
         static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-
-
-        [GlobalSetup]
-        public void GlobalSetup()
-        {
-            Console.WriteLine("// GlobalSetup");
-            AnonymizerLogging.LoggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder.AddFilter("Microsoft", LogLevel.Warning)
-                       .AddFilter("System", LogLevel.Warning)
-                       .AddFilter("Fhir.Anonymizer", LogLevel.Debug)
-                       .AddConsole();
-            });
-
-            _logger.LogDebug("// GlobalSetup from logger");
-        }
     }
 }
