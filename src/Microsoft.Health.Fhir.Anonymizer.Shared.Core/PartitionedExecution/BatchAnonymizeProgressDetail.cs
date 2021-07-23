29fe09a007335f5ee35bd1dae9fc2d88f2a81040
sowu880 <57981365+sowu880@users.noreply.github.com>
2021-07-23 13:56:59 +0800

    Add doc in batch progress details

--- src/Microsoft.Health.Fhir.Anonymizer.Shared.Core/PartitionedExecution/BatchAnonymizeProgressDetail.cs
+++ src/Microsoft.Health.Fhir.Anonymizer.Shared.Core/PartitionedExecution/BatchAnonymizeProgressDetail.cs
@@ -3,11 +3,14 @@
     public class BatchAnonymizeProgressDetail
     {
         public int CurrentThreadId { get; set; }
-
+        
+        // The number of anonymization completed resources.
         public int ProcessCompleted { get; set; }
-
+        
+        // The number of skipped resources when skipping AnonymizerProcessingException enabled.
         public int ProcessSkipped { get; set; }
-
+        
+        // Todo : this property will be removed since exception will be thrown once consuming failed.
         public int ConsumeCompleted { get; set; }
     }
 }
