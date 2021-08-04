779b25e0f648e0aaf06b6a985b23c25f3e2efb63
sowu880 <57981365+sowu880@users.noreply.github.com>
2021-08-04 13:22:30 +0800

    Merge Dicom (#128)
    * Support Dicom Anonymization (#105)
    
    * add shared de id lib
    
    * update solution
    
    * add solution for dicom
    
    * Add DICOM Anonymization CLI Tool (#107)
    
    * add dicom anonymizatin tool
    
    * update solution
    
    * update tests
    
    * update anonymizer configuration parser
    
    * update processors
    
    * update engine
    
    * update package
    
    * update project build file
    
    * update for security scan
    
    * update for security scan
    
    * update for security scan
    
    * add stylecop
    
    * solve warning
    
    * refine structure
    
    * update
    
    * refine engine
    
    * rename function name
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * refine structure (#108)
    
    * refine structure
    
    * update
    
    * refine engine
    
    * rename function name
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * update
    
    * update
    
    * update
    
    * update for perturb tests
    
    * remove DicomSettings
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix most comments
    
    * fix comments
    
    * fix tests
    
    * fix tests
    
    * fix comments
    
    * fix comments
    
    * fix null problems
    
    * add tests
    
    * update
    
    * change variable name
    
    * update config and cli tool exception handling
    
    * fix comments
    
    * fix comments
    
    * delete editorconfig
    
    * update csproj
    
    * remove useless files
    
    * update
    
    * change folder's name
    
    * update
    
    * update
    
    * update api
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * update readme

---
+++ DICOM/src/Microsoft.Health.Dicom.Anonymizer.CommandLineTool/AnonymizerLogic.cs
@@ -0,0 +1,76 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Dicom;
+using Microsoft.Health.Dicom.Anonymizer.Core;
+using Microsoft.Health.Dicom.Anonymizer.Core.Models;
+
+namespace Microsoft.Health.Dicom.Anonymizer.CommandLineTool
+{
+    internal static class AnonymizerLogic
+    {
+        internal static async Task AnonymizeAsync(AnonymizerOptions options)
+        {
+            var engine = new AnonymizerEngine(
+                    options.ConfigurationFilePath,
+                    new AnonymizerEngineOptions(options.ValidateInput, options.ValidateOutput));
+            if (options.InputFile != null && options.OutputFile != null)
+            {
+                if (IsSamePath(options.InputFile, options.OutputFile))
+                {
+                    throw new ArgumentException("Input and output file path are the same! Please check file names.");
+                }
+
+                await AnonymizeOneFileAsync(options.InputFile, options.OutputFile, engine);
+            }
+            else if (options.InputFolder != null && options.OutputFolder != null)
+            {
+                if (IsSamePath(options.InputFolder, options.OutputFolder))
+                {
+                    throw new ArgumentException("Input and output folders are the same! Please choose another folder.");
+                }
+
+                foreach (string file in Directory.EnumerateFiles(options.InputFolder, "*.dcm", SearchOption.AllDirectories))
+                {
+                    await AnonymizeOneFileAsync(file, Path.Join(options.OutputFolder, Path.GetRelativePath(options.InputFolder, file)), engine);
+                }
+
+                Console.WriteLine("Anonymization finished!");
+            }
+            else
+            {
+                throw new ArgumentException("Invalid parameters. Please specify inputFile (or inputFolder) and outputFile (or outputFolder) at the same time.\r\nSamples:\r\n [-i inputFile -o outputFile]\r\nor\r\n [-I inputFolder -O outputFolder]");
+            }
+        }
+
+        private static bool IsSamePath(string inputPath, string outputPath)
+        {
+            string inputFullPath = Path.GetFullPath(inputPath)
+                .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+            string outputFullPath = Path.GetFullPath(outputPath)
+                .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+
+            return string.Equals(inputFullPath, outputFullPath, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        internal static async Task AnonymizeOneFileAsync(string inputFile, string outputFile, AnonymizerEngine engine)
+        {
+            DicomFile dicomFile = await DicomFile.OpenAsync(inputFile).ConfigureAwait(false);
+            engine.AnonymizeDataset(dicomFile.Dataset);
+
+            if (!string.IsNullOrEmpty(Path.GetDirectoryName(outputFile)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
+            }
+
+            await dicomFile.SaveAsync(outputFile);
+
+            Console.WriteLine($"Finished processing '{inputFile}'!");
+        }
+    }
+}
---
+++ DICOM/src/Microsoft.Health.Dicom.Anonymizer.Core.UnitTests/Rules/AnonymizerMaskedTagRuleTests.cs
@@ -0,0 +1,77 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Dicom;
+using Microsoft.Health.Dicom.Anonymizer.Core.Exceptions;
+using Microsoft.Health.Dicom.Anonymizer.Core.Models;
+using Microsoft.Health.Dicom.Anonymizer.Core.Processors;
+using Microsoft.Health.Dicom.Anonymizer.Core.Rules;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.Health.Dicom.Anonymizer.Core.UnitTests.Rules
+{
+    public class AnonymizerMaskedTagRuleTests
+    {
+        public AnonymizerMaskedTagRuleTests()
+        {
+            Dataset = new DicomDataset()
+            {
+                { DicomTag.PatientAge, "100Y" },  // AS
+                { DicomTag.Parse("0008,0010"), "test" }, // SH
+                { DicomTag.Parse("0008,0012"), "20211110" }, // DA
+                { DicomTag.Parse("0008,0013"), "121212.555" }, // TM
+                { DicomTag.Parse("0008,0014"), "12345" }, // UI
+            };
+
+            MaskedTagRule = new AnonymizerMaskedTagRule(DicomMaskedTag.Parse("(0008,001x)"), "redact", "description", new DicomProcessorFactory(), JObject.Parse("{\"EnablePartialDatesForRedact\" : \"true\"}"));
+        }
+
+        public DicomDataset Dataset { get; set; }
+
+        public AnonymizerMaskedTagRule MaskedTagRule { get; set; }
+
+        [Fact]
+        public void GivenAnonymizerMaskedTagRule_WhenHandleTheRule_TheLocatedItemWillBeProcessed()
+        {
+            var context = new ProcessContext();
+            MaskedTagRule.Handle(Dataset, context);
+            Assert.True(context.VisitedNodes.Count == 4);
+            Assert.Empty(Dataset.GetString(DicomTag.Parse("0008,0010")));
+            Assert.Equal("20210101", Dataset.GetString(DicomTag.Parse("0008,0012")));
+            Assert.Empty(Dataset.GetString(DicomTag.Parse("0008,0013")));
+            Assert.Empty(Dataset.GetString(DicomTag.Parse("0008,0014")));
+        }
+
+        [Fact]
+        public void GivenAnonymizerMaskedTagRule_WhenLocateItem_AListOfResultWillBeReturned()
+        {
+            var result = MaskedTagRule.LocateDicomTag(Dataset, new ProcessContext());
+            Assert.True(result.Count == 4);
+            foreach (var item in result)
+            {
+                Assert.True(MaskedTagRule.MaskedTag.IsMatch(item.Tag));
+            }
+        }
+
+        [Fact]
+        public void GivenAnonymizerMaskedTagRule_WhenHandleTheRule_IfRuleIsNotSupportedOnItem_ExceptionWillBeThrown()
+        {
+            var newRule = new AnonymizerMaskedTagRule(DicomMaskedTag.Parse("(0008,001x)"), "perturb", "description", new DicomProcessorFactory(), JObject.Parse("{}"));
+            var context = new ProcessContext();
+            Assert.Throws<AnonymizerOperationException>(() => newRule.Handle(Dataset, context));
+        }
+
+        [Fact]
+        public void GivenAnonymizerMaskedTagRule_WhenLocateItem_VisitedItemsWillNotBeReturned()
+        {
+            var context = new ProcessContext();
+            context.VisitedNodes.Add(Dataset.GetDicomItem<DicomItem>(DicomTag.Parse("0008,0010")).ToString());
+            context.VisitedNodes.Add(Dataset.GetDicomItem<DicomItem>(DicomTag.Parse("0008,0012")).ToString());
+            var result = MaskedTagRule.LocateDicomTag(Dataset, context);
+            Assert.True(result.Count == 2);
+        }
+    }
+}
