5448cd099dc63d1c5bc61a1a1c9e6d170cc379e9
Yanhui Hong <yanhon@microsoft.com>
2022-12-20 20:47:25 +0800

    add configuration

--- Service/Microsoft.Health.DeIdentification.Local/LocalArtifactStore.cs
+++ Service/Microsoft.Health.DeIdentification.Local/LocalArtifactStore.cs
@@ -1,21 +1,37 @@
 ﻿using Microsoft.Health.DeIdentification.Contract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Health.DeIdentification.Local
 {
     public class LocalArtifactStore : IArtifactStore
     {
-        readonly string _configFile = "../configurations/deid-configuration.json";
+        readonly string _defaultConfigFile = "D:\\work\\src\\deid-server\\Service\\configurations\\deid-configuration.json";
         public LocalArtifactStore() 
         {
         }
+
+        public string DefaultConfigFile { get { return _defaultConfigFile;} }
+
         public TContent ResolveArtifact<TContent>(string reference)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var content = File.ReadAllText(reference);
+                JsonLoadSettings settings = new JsonLoadSettings
+                {
+                    DuplicatePropertyNameHandling = DuplicatePropertyNameHandling.Error
+                };
+                var token = JToken.Parse(content, settings);
+                return token.ToObject<TContent>();
+            }
+            catch (IOException innerException)
+            {
+                throw new Exception($"Failed to read file {reference}", innerException);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Failed to parse json", ex);
+            }
         }
     }
 }
---
+++ Service/Microsoft.Health.DeIdentification/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Microsoft.Health.DeIdentification.Contract
+{
+    [DataContract]
+    public class Configuration
+    {
+        [DataMember(Name = "deidConfigurations")]
+        public DeIdConfiguration[] DeidConfigurations { get; set; }
+    }
+}
