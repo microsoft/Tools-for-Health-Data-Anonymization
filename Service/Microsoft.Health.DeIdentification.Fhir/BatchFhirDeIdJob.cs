6b8e788dad9ea9e9b38bec8cdf664fc1a476d836
Yanhui Hong <yanhon@microsoft.com>
2022-12-26 13:43:54 +0800

    init job hosting

--- Service/Microsoft.Health.DeIdentification.Fhir/BatchFhirDeIdJob.cs
+++ Service/Microsoft.Health.DeIdentification.Fhir/BatchFhirDeIdJob.cs
@@ -1,7 +1,12 @@
-﻿namespace Microsoft.Health.DeIdentification.Fhir
-{
-    public class BatchFhirDeIdJob
-    {
+﻿using Microsoft.Health.JobManagement;
 
+namespace Microsoft.Health.DeIdentification.Fhir
+{
+    public class BatchFhirDeIdJob : IJob
+    {
+        public Task<string> ExecuteAsync(JobInfo jobInfo, IProgress<string> progress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
