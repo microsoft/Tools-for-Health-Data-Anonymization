00f173797f7a1c58d51df1317c4c881659762c5c
Cheng Chen <chengchen2@microsoft.com>
2022-12-29 10:29:38 +0800

    set response code

--- Service/Microsoft.Health.DeIdentification.Web/BatchResult.cs
+++ Service/Microsoft.Health.DeIdentification.Web/BatchResult.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnsureThat;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +30,21 @@ namespace Microsoft.Health.DeIdentification.Web
         public static BatchResult BadRequest()
         {
             return new BatchResult(HttpStatusCode.BadRequest);
+        }
+
+        public override Task ExecuteResultAsync(ActionContext context)
+        {
+            EnsureArg.IsNotNull(context, nameof(context));
+
+
+            HttpResponse response = context.HttpContext.Response;
+
+            response.StatusCode = (int)HttpStatusCode.Accepted;
+
+
+            ActionResult result = new EmptyResult();
+
+            return result.ExecuteResultAsync(context);
         }
     }
 }
